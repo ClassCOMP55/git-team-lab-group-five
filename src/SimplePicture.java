60e309734886882a0b6f79eb567902bb691ff8a1
tri phu truong <t_truong28@u.pacific.edu>
2026-02-13 10:32:20 -0800

    I added a comment on line 5 and 6

--- src/SimplePicture.java
+++ src/SimplePicture.java
@@ -1,7 +1,9 @@
 import acm.graphics.GImage;
 import acm.graphics.GLabel;
 import acm.program.GraphicsProgram;
-
+//i'm color blind//
+//lucas is a bully//
+//i hate him//
 public class SimplePicture extends GraphicsProgram {
 	public static int SIZE = 700;
 	private static final String robotname = "robot.png";
