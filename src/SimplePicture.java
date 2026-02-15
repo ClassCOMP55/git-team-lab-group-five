66b078a879e394ccbe6953dd356dcb31dd4e2f9a
Eduardo Montes <e_montes3@u.pacific.edu>
2026-02-15 15:57:44 -0800

    Changed line 14 and added into Line 8

--- src/SimplePicture.java
+++ src/SimplePicture.java
@@ -2,14 +2,16 @@ import acm.graphics.GImage;
 import acm.graphics.GLabel;
 import acm.program.GraphicsProgram;
 
+//We did this
 public class SimplePicture extends GraphicsProgram {
 	public static int SIZE = 700;
+	public static final String robotImage = "robot.png";
 	
 	public void init() {
 		setSize(SIZE, SIZE);
 	}
 	public void run() {
-		GImage robot = new GImage("robot.png", 200, 100);
+		GImage robot = new GImage(robotImage, 200, 100);
 		add(robot);
 		GLabel label = new GLabel("How I felt before knowing about github!", 200, 400);
 		add(label);
