1eb8c6299592c9f19d616590bf9bc26c7ac0bf7e
tri phu truong <t_truong28@u.pacific.edu>
2026-02-15 15:39:56 -0800

    ok fr fr starting from the beginning

--- src/SimplePicture.java
+++ src/SimplePicture.java
@@ -11,7 +11,7 @@ public class SimplePicture extends GraphicsProgram {
 	public void run() {
 		GImage robot = new GImage("robot.png", 200, 100);
 		add(robot);
-		GLabel label = new GLabel("How I feel when using github!", 200, 400);
+		GLabel label = new GLabel("How I felt before knowing about github!", 200, 400);
 		add(label);
 	}
 	
