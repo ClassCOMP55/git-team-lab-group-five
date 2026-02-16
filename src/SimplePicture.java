57b4c9ee1cd7f5a832e2e315f15e7e285b00d3ea
Eduardo Montes <e_montes3@u.pacific.edu>
2026-02-15 16:17:02 -0800

    just committing

--- src/SimplePicture.java
+++ src/SimplePicture.java
@@ -15,7 +15,7 @@ public class SimplePicture extends GraphicsProgram {
 	public void run() {
 		GImage robot = new GImage(robotImage, 200, 100);
 		add(robot);
-		GLabel label = new GLabel("I love COMP 055!", 200, 400);
+		GLabel label = new GLabel("I don't love COMP 055!", 200, 400);
 		add(label);
 	}
 	
